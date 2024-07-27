2d4eb071f3ee38f76d27d797569e6c73bca51d4b
sadiquecode <muhammadsadique116@gmail.com>
2024-07-27 17:54:41 +0500

    asas

--- job_design/assets/js/main.js
+++ job_design/assets/js/main.js
@@ -7,3 +7,24 @@ function updateLabel(inputId, spanId) {
         fileNameSpan.textContent = 'No file uploaded';
     }
 }
+
+// JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
